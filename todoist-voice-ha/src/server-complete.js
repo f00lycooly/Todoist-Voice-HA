4cbd3c4814fbac97ae353ecfd524156a979dc0cc
foolycooly <bagofholding79@gmail.com>
2025-07-13 20:06:14 +0100

    Clean up and finalize add-on with complete server implementation

--- todoist-voice-ha/src/server-complete.js
+++
@@ -1,3 +0,0 @@
-// Complete server implementation goes here
-// Copy from the "Enhanced Server.js with HA Services Integration" artifact
-console.log("TODO: Add complete server implementation from artifacts");
